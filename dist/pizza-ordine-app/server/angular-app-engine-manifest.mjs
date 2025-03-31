847d74e03bc9d189b7c69373b55d9ca029cc1b0d
giorgiomassi89 <giorgiomassi89@gmail.com>
2025-03-31 18:46:45 +0000

    pages github

--- dist/pizza-ordine-app/server/angular-app-engine-manifest.mjs
+++ dist/pizza-ordine-app/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/nanosystem',
+  basePath: '/nanosystem/src',
   supportedLocales: {
   "en-US": ""
 },
--- dist/pizza-ordine-app/server/angular-app-manifest.mjs
+++ dist/pizza-ordine-app/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/nanosystem/',
+  baseHref: '/nanosystem/src/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/nanosystem"
+    "route": "/nanosystem/src"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 9475, hash: '5f30324b7f466f202b3d56e34f370ba0ada1dab6b2b9436a9fea1000efb39661', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2858, hash: '132bad23e484356f014c55f53a20a5f1ee09f605ad095abb88bf0359f659595e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 148924, hash: 'd0cba2028b6bd1bf9738f4f7fe4956545ec502b7dd5119e06b4b7a34fdec59ef', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 9479, hash: 'e0a937e77209d9b31ea0c1d517180dcff8d80801a385f52746237fb77f8a0f41', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2862, hash: '3e1ea5a816a5e093a1a61838bdffaea0e0a3b615b677acb080024b89bf55bbf7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 148928, hash: '17ca30cff51b227362fa33620003ee9325e62722aa3c1d3ab299766be118a94b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-G6YZ6KIN.css': {size: 8082, hash: 'ppRRh7G4ddY', text: () => import('./assets-chunks/styles-G6YZ6KIN_css.mjs').then(m => m.default)}
   },
 };
