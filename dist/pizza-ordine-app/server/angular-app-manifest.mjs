7b12dd8e3cc1c1edc7bfea28502e5df4ee4b916a
giorgiomassi89 <giorgiomassi89@gmail.com>
2025-03-31 20:55:04 +0000

    Update pages github

--- dist/pizza-ordine-app/server/angular-app-manifest.mjs
+++ dist/pizza-ordine-app/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 9475, hash: 'e223062eec14be46cc47ed6d5af3641abb2865e0223d23fca87f673f8df70ea9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2858, hash: 'ba653799981bd37687a776cb182a0a8c410d1005a52101547ccdbfd06d684da4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 150751, hash: 'fa395cc1d4d3762ee0d5c03945a4223a42b18180b17914ff2bad32f1e6ffcf25', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 9475, hash: 'c09b530dd543ff7d284cd9a8ba74e29042ce916b2bba0b87503cfdfb811411e9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2858, hash: '6be92370834a091e6ae9489254cce3d584eda14843a175d20fbbb8ef8b18d412', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 150754, hash: '8deaf4366ecd6f899569a62e6ec9946b5e20c875ba10d9a56b6ba1877df25f63', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-G6YZ6KIN.css': {size: 8082, hash: 'ppRRh7G4ddY', text: () => import('./assets-chunks/styles-G6YZ6KIN_css.mjs').then(m => m.default)}
   },
 };
